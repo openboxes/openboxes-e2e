7f3048c2faa54767a0f7b3340967f32e1f84a843
Darek Rodzewicz <DRodzewicz@gmail.com>
2024-03-27 13:46:39 +0100

    OBPIH-6216 Refactored Navigation fixture to include URL prefix

--- src/utils/Navigation.ts
+++ src/utils/Navigation.ts
@@ -2,17 +2,23 @@ import { Page } from "@playwright/test";
 
 class Navigation {
     private page: Page;
+    
+    private URL_PREFIX = "/openboxes";
 
     constructor(page: Page) {
         this.page = page;
     }
 
+    private buildURL(path: string) {
+        return `${this.URL_PREFIX}${path}`
+    }
+
     async goToHome() {
-        await this.page.goto("./");
+        await this.page.goto(this.buildURL("/"));
     }
 
     async goToDashboard() {
-        await this.page.goto("./dashboard");
+        await this.page.goto(this.buildURL("/dashboard"));
     }
 }
 
