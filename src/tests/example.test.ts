baca8c8444b00df50a5c3df1050627cb3c3f4a85
Darek Rodzewicz <DRodzewicz@gmail.com>
2024-03-27 10:12:35 +0100

    OBPIH-6216 Setup environment variables for testing

--- src/tests/example.test.ts
+++ src/tests/example.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe("example test", () => {
   test("example", async ({ page }) => {
-    await page.goto('http://localhost:8080/openboxes/')
+    await page.goto('./')
     expect(true).toBeTruthy();
   });
 })
---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,28 @@
+import env from "env-var";
+
+class AppConfig {
+  private static configInstance: AppConfig;
+
+  // Base URL to use in actions like `await page.goto('./dashboard')`.
+  public appURL!: string;
+
+  // Are tests running in Continuous Integration
+  public isCI!: boolean;
+
+  private constructor() {}
+
+  public static get instance(): AppConfig {
+    if (!AppConfig.configInstance) {
+      AppConfig.configInstance = new AppConfig();
+    }
+
+    return AppConfig.configInstance;
+  }
+
+  public initialize() {
+      this.appURL = env.get("APP_BASE_URL").required().asString();
+      this.isCI = env.get("CI").default("false").asBool();
+  }
+}
+
+export default AppConfig;
