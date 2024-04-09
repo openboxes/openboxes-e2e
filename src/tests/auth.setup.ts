2abe306934050172fec1fdd40c50b694d7d17e9b
Darek Rodzewicz <DRodzewicz@gmail.com>
2024-04-09 10:11:07 +0200

    OBPIH-6217 Validate all required roles on a test user in setup file

--- src/tests/auth.setup.ts
+++ src/tests/auth.setup.ts
@@ -1,13 +1,22 @@
 import { test } from '@/fixtures/fixtures';
 import AppConfig from '@/utils/AppConfig';
+import { assertAllRequiredRoles } from '@/utils/roleUtils';
 
-test('authenticate', async ({ page, loginPage }) => {
+const testingUser = AppConfig.instance.user;
+
+// eslint-disable-next-line playwright/expect-expect
+test('authenticate', async ({ page, loginPage, genericService }) => {
   await loginPage.goToPage();
-
-  const testingUser = AppConfig.instance.user;
 
   await loginPage.fillLoginForm(testingUser.username, testingUser.password);
   await loginPage.loginButton.click();
 
   await page.context().storageState({ path: testingUser.storagePath });
+
+  const { data } = await genericService.getAppContext();
+  
+  const userGlobalRoles = data?.user?.roles || [];
+  const currentLocationRoles = data?.currentLocationRoles || [];
+  const allUserRoles = new Set([...userGlobalRoles, ...currentLocationRoles]);
+  assertAllRequiredRoles(allUserRoles, testingUser.requiredRoles)
 });
