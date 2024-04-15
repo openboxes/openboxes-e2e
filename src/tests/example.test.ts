e9f58048134722b49b4f8d16984f13e7be5852c5
Darek Rodzewicz <DRodzewicz@gmail.com>
2024-04-15 14:45:27 +0200

    OBPIH-6218 Update example test

--- src/tests/example.test.ts
+++ src/tests/example.test.ts
@@ -1,6 +1,4 @@
-import { expect } from '@playwright/test';
-
-import { test } from '@/fixtures/fixtures';
+import { expect, test } from '@/fixtures/fixtures';
 import AppConfig from '@/utils/AppConfig';
 
 test.describe('example test for main testing user', () => {
@@ -18,6 +16,41 @@ test.describe('example test for requestor testing user', () => {
   test.use({ storageState: AppConfig.instance.users['requestor'].storagePath });
   test('Open dashboard page as a requestor user', async ({ page, navbar }) => {
     await page.goto('./dashboard');
+    await navbar.profileButton.click();
+    await expect(navbar.navbar).toContainText(
+      AppConfig.instance.users['requestor'].username,
+      { ignoreCase: true }
+    );
+  });
+
+  test('Open dashboard page as a main test user in ward location', async ({
+    page,
+    navbar,
+    locationChooser,
+    wardLocation,
+  }) => {
+    await page.goto('./dashboard');
+
+    await navbar.locationChooserButton.click();
+
+    await locationChooser
+      .getOrganization(wardLocation.organization?.name)
+      .click();
+    await locationChooser.getLocation(wardLocation.name).click();
+
+    await navbar.profileButton.click();
+    await expect(navbar.navbar).toContainText(
+      AppConfig.instance.users['requestor'].username,
+      { ignoreCase: true }
+    );
+  });
+
+  test('Open dashboard page as a main test user in ward location 2', async ({
+    page,
+    navbar,
+  }) => {
+    await page.goto('./dashboard');
+
     await navbar.profileButton.click();
     await expect(navbar.navbar).toContainText(
       AppConfig.instance.users['requestor'].username,
