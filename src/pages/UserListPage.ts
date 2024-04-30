bcb615ec15d0ee4a55992886ad0b8f1c892574d2
User <kkrawczyk@soldevelo.com>
2024-04-30 15:40:21 +0200

    OBPIH-6342: fix selectors for UserListPage

--- src/pages/UserListPage.ts
+++ src/pages/UserListPage.ts
@@ -2,29 +2,29 @@ import { expect } from '@/fixtures/fixtures';
 import BasePageModel from '@/pages/BasePageModel';
 
 class UserListPage extends BasePageModel {
-    async isLoaded() {
-        await expect(this.page.getByText('Users')).toBeVisible();
-      }
-
-      get createUserButton() {
-        return this.page.getByText('Create User');
-      }
-
-      get searchByName() {
-        return this.page.getByLabel('filter-by-name');
-      }
-
-      get findButton() {
-        return this.page.getByRole('button', {name: 'Find'});
-      }
-
-      get userListTable() {
-        return this.page.getByRole('table');
-      }
-
-      getUserToEdit(username: string) {
-        return this.page.getByRole('link').getByText(username);
-      }
+  async isLoaded() {
+    await expect(this.page.getByText('Users')).toBeVisible();
   }
-  
-  export default UserListPage;
+
+  get createUserButton() {
+    return this.page.getByText('Create User');
+  }
+
+  get searchByNameField() {
+    return this.page.getByRole('textbox', { name: 'filter-by-name' });
+  }
+
+  get findButton() {
+    return this.page.getByRole('button', { name: 'Find' });
+  }
+
+  get userListTable() {
+    return this.page.getByRole('table');
+  }
+
+  getUserToEdit(username: string) {
+    return this.page.getByRole('link').getByText(username);
+  }
+}
+
+export default UserListPage;
