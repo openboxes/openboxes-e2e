c5daab2b9795c0ceefd8a8912987a1e61aa081bc
User <kkrawczyk@soldevelo.com>
2024-04-30 15:33:52 +0200

    OBPIH-6342: fix selectors for CreateUserPage and add fillUserForm method

--- src/pages/CreateUserPage.ts
+++ src/pages/CreateUserPage.ts
@@ -1,31 +1,44 @@
 import { expect } from '@/fixtures/fixtures';
 import BasePageModel from '@/pages/BasePageModel';
+import { UserType } from '@/types';
 
 class CreateUserPage extends BasePageModel {
-    async isLoaded() {
-        await expect(this.page.getByText('Username')).toBeVisible();
-      }
-
-      get username() {
-        return this.page.getByRole('textbox', { name: 'username' });
-      }
-
-      get firstName() {
-        return this.page.getByRole('row').filter({ hasText: 'First name' }).getByRole('textbox');
-      }
-
-      get lastName() {
-        return this.page.getByRole('row').filter({ hasText: 'Last name' }).getByRole('textbox');
-      }
-
-      get password() {
-        return this.page.getByRole('textbox', { name: 'password' });
-      }
-
-      get saveButton() {
-        return this.page.getByRole('button', {name: 'Save'});
-      }
-  
+  async isLoaded() {
+    await expect(this.page.getByText('Username')).toBeVisible();
   }
-  
-  export default CreateUserPage;
+
+  get usernameField() {
+    return this.page.getByRole('textbox', { name: 'username' });
+  }
+
+  get firstNameField() {
+    return this.page
+      .getByRole('row')
+      .filter({ hasText: 'First name' })
+      .getByRole('textbox');
+  }
+
+  get lastNameField() {
+    return this.page
+      .getByRole('row')
+      .filter({ hasText: 'Last name' })
+      .getByRole('textbox');
+  }
+
+  get passwordField() {
+    return this.page.getByRole('textbox', { name: 'password' });
+  }
+
+  get saveButton() {
+    return this.page.getByRole('button', { name: 'Save' });
+  }
+
+  async fillUserForm(data: UserType) {
+    await this.usernameField.fill(data.username);
+    await this.firstNameField.fill(data.firstName);
+    await this.lastNameField.fill(data.lastName);
+    await this.passwordField.fill(data.password);
+  }
+}
+
+export default CreateUserPage;
