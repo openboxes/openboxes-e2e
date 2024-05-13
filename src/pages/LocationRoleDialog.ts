0a6347d35b54a5bcf55ad3926d5532208bb4b633
User <kkrawczyk@soldevelo.com>
2024-05-13 11:13:01 +0200

    OBPIH-6342: add location role dialog

---
+++ src/pages/LocationRoleDialog.ts
@@ -0,0 +1,35 @@
+import BasePageModel from '@/pages/BasePageModel';
+
+class LocationRoleDialog extends BasePageModel {
+  get locationSelectClearButton() {
+    return this.locationForLocationRoleSelect.locator('.search-choice-close');
+  }
+
+  get locationForLocationRoleSelect() {
+    return this.page.getByTestId('location-select');
+  }
+
+  getLocationForLocationRole(name: string) {
+    return this.locationForLocationRoleSelect
+      .getByRole('listitem')
+      .getByText(name, { exact: true });
+  }
+
+  get locationRoleSelect() {
+    return this.page.getByTestId('role-select');
+  }
+
+  getUserLocationRole(role: string) {
+    return this.locationRoleSelect
+      .getByRole('list')
+      .getByText(role, { exact: true });
+  }
+
+  get saveButtonOnLocationRoleDialog() {
+    return this.page
+      .getByRole('dialog', { name: 'Add Location Roles' })
+      .getByRole('button', { name: 'Save' });
+  }
+}
+
+export default LocationRoleDialog;
