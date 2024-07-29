7561b75265e995da105c2ec96c16407e9c2a7239
Darek Rodzewicz <DRodzewicz@gmail.com>
2024-07-29 13:17:55 +0200

    OBPIH-6448 Add test for switching locations

--- src/tests/inbound/inboudList.test.ts
+++ src/tests/inbound/inboudList.test.ts
@@ -902,4 +902,156 @@ test.describe('Inbond Stock Movement list page', () => {
       ).not.toContainText(formatDate(filters.createdBefore));
     });
   });
+
+  test('Switch locations filters', async ({
+    inboundListPage,
+    supplierLocation,
+    mainLocation,
+    depotLocation,
+    genericService,
+    locationChooser,
+    stockMovementService,
+    navbar
+  }) => {
+    const mainLocationLocation = await mainLocation.getLocation();
+    const depotLocationLocation = await depotLocation.getLocation();
+    const supplierLocationLocation = await supplierLocation.getLocation();
+    const user = await genericService.getLoggedInUser();
+    const TODAY = getToday();
+
+    const STOCK_MOVEMENT = await stockMovementService.createInbound({
+      originId: supplierLocationLocation.id,
+    });
+
+    const filters = {
+      search: 'TEST',
+      receiptStatus: 'Created',
+      destination: mainLocationLocation.name,
+      origin: supplierLocationLocation.name,
+      shipmentType: 'Air',
+      requestedBy: user.name,
+      createdBy: user.name,
+      updatedBy: user.name,
+      createdAfter: TODAY,
+      createdBefore: TODAY,
+    };
+
+    await test.step('Go to inbound list page', async () => {
+      await inboundListPage.goToPage();
+    });
+
+    await test.step('Assert that created stock movement is visible in the table', async () => {
+      await expect(inboundListPage.table.table).toContainText(STOCK_MOVEMENT.identifier);
+    });
+
+    await test.step('Fill search filter', async () => {
+      await inboundListPage.filters.searchField.textbox.fill(filters.search);
+    });
+
+    await test.step('Fill receipt status filter', async () => {
+      await inboundListPage.filters.receiptStatusSelect.click();
+      await inboundListPage.filters.receiptStatusSelect
+        .getSelectOption(filters.receiptStatus)
+        .click();
+    });
+
+    await test.step('Fill origin filter', async () => {
+      await inboundListPage.filters.originSelect.findAndSelectOption(
+        supplierLocationLocation.name
+      );
+    });
+
+    await test.step('Fill shipment type filter', async () => {
+      await inboundListPage.filters.shipmentTypeSelect.click();
+      await inboundListPage.filters.shipmentTypeSelect
+        .getSelectOption(filters.shipmentType)
+        .click();
+    });
+
+    await test.step('Fill requested by filter', async () => {
+      await inboundListPage.filters.requestedBySelect.findAndSelectOption(
+        user.name
+      );
+    });
+
+    await test.step('Fill created by filter', async () => {
+      await inboundListPage.filters.createdBySelect.findAndSelectOption(
+        user.name
+      );
+    });
+
+    await test.step('Fill updated by filter', async () => {
+      await inboundListPage.filters.updatedBySelect.findAndSelectOption(
+        user.name
+      );
+    });
+
+    await test.step('Fill created Before filter', async () => {
+      await inboundListPage.filters.createdBeforeDateFilter.click();
+      await inboundListPage.filters.createdBeforeDateFilter
+        .getMonthDay(getDayOfMonth(filters.createdBefore))
+        .click();
+    });
+
+    await test.step('Fill created after filter', async () => {
+      await inboundListPage.filters.createdAfterDateFilter.click();
+      await inboundListPage.filters.createdAfterDateFilter
+        .getMonthDay(getDayOfMonth(filters.createdAfter))
+        .click();
+    });
+
+    await test.step('Assert that all filters are filled', async () => {
+      await expect(inboundListPage.filters.searchField.textbox).toHaveValue(
+        filters.search
+      );
+      await expect(
+        inboundListPage.filters.receiptStatusSelect.countIndicator
+      ).toBeVisible();
+      await expect(inboundListPage.filters.originSelect.field).toContainText(
+        filters.origin
+      );
+      await expect(
+        inboundListPage.filters.shipmentTypeSelect.countIndicator
+      ).toBeVisible();
+      await expect(
+        inboundListPage.filters.requestedBySelect.field
+      ).toContainText(filters.requestedBy);
+      await expect(inboundListPage.filters.createdBySelect.field).toContainText(
+        filters.createdBy
+      );
+      await expect(inboundListPage.filters.updatedBySelect.field).toContainText(
+        filters.updatedBy
+      );
+      await expect(
+        inboundListPage.filters.createdAfterDateFilter.field
+      ).toContainText(formatDate(filters.createdAfter));
+      await expect(
+        inboundListPage.filters.createdBeforeDateFilter.field
+      ).toContainText(formatDate(filters.createdBefore));
+    });
+
+    await test.step('Assert that destination filter is fillted with current logged in location', async () => {
+      await expect(
+        inboundListPage.filters.destinationSelect.field
+      ).toContainText(mainLocationLocation.name);
+    });
+
+    await test.step('Switch to other depot location', async () => {
+      await navbar.locationChooserButton.click();
+      await locationChooser
+        .getOrganization(depotLocationLocation.organization?.name as string)
+        .click();
+      await locationChooser.getLocation(depotLocationLocation.name).click();
+    })
+
+    await test.step('Assert that created stock movement is visible in the table', async () => {
+      await expect(inboundListPage.table.table).not.toContainText(STOCK_MOVEMENT.identifier);
+    });
+
+    await test.step('Assert that destination filter is fillted with current logged in location', async () => {
+      await expect(
+        inboundListPage.filters.destinationSelect.field
+      ).toContainText(depotLocationLocation.name);
+    });
+  });
 });
