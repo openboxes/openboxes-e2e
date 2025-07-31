2e4ff3b62deecd7b4bbe1d034f5f2f391eb8e5a5
Alan Nadolny <alannadolnyy@gmail.com>
2025-07-31 18:10:56 +0200

    OBPIH-6971 Add unflatten util

--- src/utils/ServiceUtils.ts
+++ src/utils/ServiceUtils.ts
@@ -1,4 +1,5 @@
 import { APIResponse } from '@playwright/test';
+import _ from 'lodash'
 
 export async function parseRequestToJSON(response: APIResponse) {
   try {
@@ -6,4 +7,14 @@ export async function parseRequestToJSON(response: APIResponse) {
   } catch (error) {
     throw new Error('Problem parsing JSON');
   }
+}
+
+
+export function unflatten(obj: object) {
+  const result = {};
+  _.forOwn(obj, (value, key) => {
+    _.set(result, key, value);
+  });
+
+  return result;
 }
