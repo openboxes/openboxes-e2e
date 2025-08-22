0362ec7a05a94fdc299cb3a8da91e5a2735a8b88
Alan Nadolny <alannadolnyy@gmail.com>
2025-08-22 14:21:18 +0200

    OBPIH-6969 Add parsing json to csv

--- src/utils/ServiceUtils.ts
+++ src/utils/ServiceUtils.ts
@@ -18,3 +18,19 @@ export function unflatten(obj: object) {
 
   return result;
 }
+
+export function jsonToCsv(data: Record<string, string>[]): string {
+  if (!Array.isArray(data) || !data.length) {
+    throw new Error('Input JSON array is empty');
+  }
+
+  const headers = Object.keys(data[0]);
+  const csvRows = [
+    headers.join(','),
+    ...data.map(row => headers.map(header =>
+      JSON.stringify(row[header] ?? '')).join(',')
+    ),
+  ];
+
+  return csvRows.join('\n');
+}
