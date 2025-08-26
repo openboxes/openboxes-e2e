eba4871cad218259481117c4061615ce63d7ade9
Alan Nadolny <alannadolnyy@gmail.com>
2025-08-26 14:34:43 +0200

    OBPIH-6969 Replace hardcoded products with date from csv file

--- src/setup/dataImport.setup.ts
+++ src/setup/dataImport.setup.ts
@@ -7,6 +7,11 @@ test('import data', async ({ request }) => {
   // eslint-disable-next-line playwright/no-conditional-in-test
   const data = readFile(AppConfig.TEST_DATA_FILE_PATH) || {};
 
+  const seedData: Record<'products', Record<string, string>> = {
+    ...data,
+    products: {},
+  };
+
   // PRODUCTS
   const productService = new ProductService(request);
 
@@ -15,9 +20,9 @@ test('import data', async ({ request }) => {
   await test.step(`importing ${productsData.length} products`, async () => {
     const importedData = await productService.importProducts(productsData);
     importedData.data.forEach((product) => {
-      data.products[product.productCode] = product.id;
+      seedData.products[product.productCode] = product.id;
     })
   })
 
-  writeToFile(AppConfig.TEST_DATA_FILE_PATH, data);
+  writeToFile(AppConfig.TEST_DATA_FILE_PATH, seedData);
 });
