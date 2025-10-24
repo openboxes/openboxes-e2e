eeca9846f68391b81ce16a323a431a506729b1e9
Katarzyna <kkrawczyk@soldevelo.com>
2025-10-24 12:45:00 +0200

    add test for rollback last receipt behavior

---
+++ src/tests/putaway/rollbackLastReceiptWhenPutawayCreated.test.ts
@@ -0,0 +1,179 @@
+import AppConfig from '@/config/AppConfig';
+import { ShipmentType } from '@/constants/ShipmentType';
+import { expect, test } from '@/fixtures/fixtures';
+import { StockMovementResponse } from '@/types';
+import { getShipmentId, getShipmentItemId } from '@/utils/shipmentUtils';
+
+test.describe('Rollback last receipt behavior when putaway created', () => {
+  let STOCK_MOVEMENT: StockMovementResponse;
+
+  test.beforeEach(
+    async ({
+      supplierLocationService,
+      stockMovementService,
+      fifthProductService,
+      receivingService,
+    }) => {
+      const supplierLocation = await supplierLocationService.getLocation();
+      STOCK_MOVEMENT = await stockMovementService.createInbound({
+        originId: supplierLocation.id,
+      });
+
+      const product = await fifthProductService.getProduct();
+
+      await stockMovementService.addItemsToInboundStockMovement(
+        STOCK_MOVEMENT.id,
+        [{ productId: product.id, quantity: 10 }]
+      );
+
+      await stockMovementService.sendInboundStockMovement(STOCK_MOVEMENT.id, {
+        shipmentType: ShipmentType.AIR,
+      });
+
+      const { data: stockMovement } =
+        await stockMovementService.getStockMovement(STOCK_MOVEMENT.id);
+      const shipmentId = getShipmentId(stockMovement);
+      const { data: receipt } = await receivingService.getReceipt(shipmentId);
+      const receivingBin =
+        AppConfig.instance.receivingBinPrefix + STOCK_MOVEMENT.identifier;
+
+      await receivingService.createReceivingBin(shipmentId, receipt);
+
+      await receivingService.updateReceivingItems(shipmentId, [
+        {
+          shipmentItemId: getShipmentItemId(receipt, 0, 0),
+          quantityReceiving: 10,
+          binLocationName: receivingBin,
+        },
+      ]);
+      await receivingService.completeReceipt(shipmentId);
+    }
+  );
+
+  test.afterEach(
+    async ({
+      stockMovementShowPage,
+      stockMovementService,
+      navbar,
+      transactionListPage,
+      oldViewShipmentPage,
+    }) => {
+      await navbar.configurationButton.click();
+      await navbar.transactions.click();
+      await transactionListPage.table.row(1).actionsButton.click();
+      await transactionListPage.table.deleteButton.click();
+      await expect(transactionListPage.successMessage).toBeVisible();
+      await transactionListPage.table.row(1).actionsButton.click();
+      await transactionListPage.table.deleteButton.click();
+      await expect(transactionListPage.successMessage).toBeVisible();
+      await stockMovementShowPage.goToPage(STOCK_MOVEMENT.id);
+      await stockMovementShowPage.detailsListTable.oldViewShipmentPage.click();
+      await oldViewShipmentPage.undoStatusChangeButton.click();
+      await stockMovementShowPage.isLoaded();
+      await stockMovementShowPage.rollbackButton.click();
+      await stockMovementService.deleteStockMovement(STOCK_MOVEMENT.id);
+    }
+  );
+
+  test('Rollback last receipt behavior when putaway created', async ({
+    stockMovementShowPage,
+    navbar,
+    createPutawayPage,
+    internalLocationService,
+    receivingPage,
+    putawayListPage,
+    putawayDetailsPage,
+  }) => {
+    await test.step('Go to stock movement show page and assert received status', async () => {
+      await stockMovementShowPage.goToPage(STOCK_MOVEMENT.id);
+      await stockMovementShowPage.isLoaded();
+      await expect(stockMovementShowPage.statusTag).toHaveText('Received');
+      await navbar.profileButton.click();
+      await navbar.refreshCachesButton.click();
+    });
+
+    await test.step('Go to create putaway page', async () => {
+      await navbar.inbound.click();
+      await navbar.createPutaway.click();
+      await createPutawayPage.isLoaded();
+    });
+
+    await test.step('Start putaway', async () => {
+      await createPutawayPage.table.row(0).checkbox.click();
+      await createPutawayPage.startPutawayButton.click();
+      await createPutawayPage.startStep.isLoaded();
+    });
+
+    await test.step('Select bin to putaway', async () => {
+      const internalLocation = await internalLocationService.getLocation();
+      await createPutawayPage.startStep.table.row(0).putawayBinSelect.click();
+      await createPutawayPage.startStep.table
+        .row(0)
+        .getPutawayBin(internalLocation.name)
+        .click();
+    });
+
+    await test.step('Save progress on pending putaway', async () => {
+      await createPutawayPage.startStep.saveButton.click();
+    });
+
+    await test.step('Go to stock movement show page and rollback last receipt when pending putaway created', async () => {
+      await stockMovementShowPage.goToPage(STOCK_MOVEMENT.id);
+      await stockMovementShowPage.isLoaded();
+      await expect(stockMovementShowPage.statusTag).toHaveText('Received');
+      await stockMovementShowPage.rollbackLastReceiptButton.click();
+      await expect(stockMovementShowPage.statusTag).toHaveText('Shipped');
+    });
+
+    await test.step('Receive sm', async () => {
+      await stockMovementShowPage.receiveButton.click();
+      await receivingPage.receivingStep.isLoaded();
+      await receivingPage.receivingStep.autofillQuantitiesButton.click();
+      await receivingPage.nextButton.click();
+      await receivingPage.checkStep.isLoaded();
+      await receivingPage.checkStep.receiveShipmentButton.click();
+      await stockMovementShowPage.isLoaded();
+      await expect(stockMovementShowPage.statusTag).toHaveText('Received');
+    });
+
+    await test.step('Go to putaway list page', async () => {
+      await navbar.inbound.click();
+      await navbar.listPutaways.click();
+      await putawayListPage.isLoaded();
+    });
+
+    await test.step('Open putaway detials page', async () => {
+      await putawayListPage.table.row(1).actionsButton.click();
+      await putawayListPage.table.viewOrderDetailsButton.click();
+      await putawayDetailsPage.isLoaded();
+    });
+
+    await test.step('Edit pending putaway', async () => {
+      await putawayDetailsPage.editButton.click();
+      await createPutawayPage.startStep.isLoaded();
+      await createPutawayPage.startStep.nextButton.click();
+    });
+
+    await test.step('Go to next page and complete putaway', async () => {
+      await createPutawayPage.completeStep.isLoaded();
+      await createPutawayPage.completeStep.completePutawayButton.click();
+    });
+
+    await test.step('Assert completing putaway', async () => {
+      await putawayDetailsPage.isLoaded();
+      await expect(putawayDetailsPage.statusTag).toHaveText('Completed');
+    });
+
+    await test.step('Go to stock movement show page and rollback last receipt when completed putaway created', async () => {
+      await stockMovementShowPage.goToPage(STOCK_MOVEMENT.id);
+      await stockMovementShowPage.isLoaded();
+      await expect(stockMovementShowPage.statusTag).toHaveText('Received');
+      await stockMovementShowPage.rollbackLastReceiptButton.click();
+      await expect(stockMovementShowPage.informationMessage).toBeVisible();
+      await expect(stockMovementShowPage.informationMessage).toContainText(
+        'Unable to rollback last receipt in stock movement'
+      );
+      await expect(stockMovementShowPage.statusTag).toHaveText('Received');
+    });
+  });
+});
