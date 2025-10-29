4ca4b2b69873614ea3186269469b45dd8f7073a1
Katarzyna <kkrawczyk@soldevelo.com>
2025-10-29 10:18:41 +0100

    add delete transaction method

--- src/pages/transactions/TransactionListPage.ts
+++ src/pages/transactions/TransactionListPage.ts
@@ -1,4 +1,4 @@
-import { Page } from '@playwright/test';
+import { expect, Page } from '@playwright/test';
 
 import BasePageModel from '@/pages/BasePageModel';
 
@@ -13,6 +13,12 @@ class TransactionListPage extends BasePageModel {
 
   get successMessage() {
     return this.page.locator('.message');
+  }
+
+  async getDeleteTransaction(n: number) {
+    await this.table.row(n).actionsButton.click();
+    await this.table.deleteButton.click();
+    await expect(this.page.locator('.message')).toBeVisible();
   }
 }
 
