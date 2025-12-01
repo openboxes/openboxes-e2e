69eb74387e2a541593e364633b012bc9dbabf038
Katarzyna <kkrawczyk@soldevelo.com>
2025-12-01 15:51:51 +0100

    add elements to putaway details page

--- src/pages/putaway/putawayDetails/PutawayDetailsPage.ts
+++ src/pages/putaway/putawayDetails/PutawayDetailsPage.ts
@@ -2,14 +2,20 @@ import { expect, Page } from '@playwright/test';
 
 import BasePageModel from '@/pages/BasePageModel';
 
+import ItemStatusTable from './components/ItemStatusTable';
+import OrderHeaderTable from './components/OrderHeaderTable';
 import SummaryTable from './components/SummaryTable';
 
 class PutawayDetailsPage extends BasePageModel {
   summaryTable: SummaryTable;
+  orderHeaderTable: OrderHeaderTable;
+  itemStatusTable: ItemStatusTable;
 
   constructor(page: Page) {
     super(page);
     this.summaryTable = new SummaryTable(page);
+    this.orderHeaderTable = new OrderHeaderTable(page);
+    this.itemStatusTable = new ItemStatusTable(page);
   }
 
   async isLoaded() {
@@ -18,6 +24,40 @@ class PutawayDetailsPage extends BasePageModel {
 
   get summary() {
     return this.page.locator('#order-summary');
+  }
+
+  get summaryActionsButton() {
+    return this.summary.getByRole('button');
+  }
+
+  get actionsViewOrderDetailsButton() {
+    return this.page
+      .locator('.action-menu-item')
+      .getByRole('link', { name: 'View order details' });
+  }
+
+  get actionsAddCommentButton() {
+    return this.page
+      .locator('.action-menu-item')
+      .getByRole('link', { name: 'Add comment' });
+  }
+
+  get actionsAddDocumentsButton() {
+    return this.page
+      .locator('.action-menu-item')
+      .getByRole('link', { name: 'Add document' });
+  }
+
+  get actionsGeneratePutawayListButton() {
+    return this.page
+      .locator('.action-menu-item')
+      .getByRole('link', { name: 'Generate Putaway List' });
+  }
+
+  get actionsDeleteButton() {
+    return this.page
+      .locator('.action-menu-item')
+      .getByRole('link', { name: 'Delete Order' });
   }
 
   get statusTag() {
@@ -50,8 +90,28 @@ class PutawayDetailsPage extends BasePageModel {
     return this.page.getByRole('link', { name: 'List Orders' });
   }
 
+  get createOrderButton() {
+    return this.page.getByRole('link', { name: 'Create Order' });
+  }
+
+  get showOrderButton() {
+    return this.page.getByRole('link', { name: 'Show Order' });
+  }
+
   get editButton() {
     return this.page.getByRole('link', { name: 'Edit Putaway' });
+  }
+
+  get addCommentButton() {
+    return this.page.getByRole('link', { name: 'Add comment' });
+  }
+
+  get addDocumentButton() {
+    return this.page.getByRole('link', { name: 'Add document' });
+  }
+
+  get generatePutawayListButton() {
+    return this.page.getByRole('link', { name: 'Generate Putaway List' });
   }
 }
 
---
+++ src/pages/putaway/putawayDetails/components/ItemStatusTable.ts
@@ -0,0 +1,53 @@
+import { Locator, Page } from '@playwright/test';
+
+import BasePageModel from '@/pages/BasePageModel';
+
+class ItemStatusTable extends BasePageModel {
+  constructor(page: Page) {
+    super(page);
+  }
+
+  get table() {
+    return this.page.getByRole('table');
+  }
+
+  get rows() {
+    return this.table.getByRole('cell');
+  }
+
+  row(index: number) {
+    return new Row(this.page, this.rows.nth(index));
+  }
+
+  getColumnHeader(columnName: string) {
+    return this.table.getByRole('row').getByText(columnName, { exact: true });
+  }
+}
+
+class Row extends BasePageModel {
+  row: Locator;
+  constructor(page: Page, row: Locator) {
+    super(page);
+    this.row = row;
+  }
+
+  get itemStatus() {
+    return this.page
+      .getByTestId('item-status-table')
+      .getByTestId('order-item-status-code');
+  }
+
+  get originBin() {
+    return this.page
+      .getByTestId('item-status-table')
+      .getByTestId('origin-bin-location');
+  }
+
+  get destinationBin() {
+    return this.page
+      .getByTestId('item-status-table')
+      .getByTestId('destination-bin-location');
+  }
+}
+
+export default ItemStatusTable;
