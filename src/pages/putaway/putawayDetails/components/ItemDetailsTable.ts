21a9a1fbf062a2a76f358630a8e6681e41695e13
Katarzyna <kkrawczyk@soldevelo.com>
2026-04-22 12:24:43 +0200

    add item details table to putaway view page

---
+++ src/pages/putaway/putawayDetails/components/ItemDetailsTable.ts
@@ -0,0 +1,31 @@
+import { Locator, Page } from '@playwright/test';
+
+import BasePageModel from '@/pages/BasePageModel';
+
+class ItemDetailsTable extends BasePageModel {
+  constructor(page: Page) {
+    super(page);
+  }
+
+  get table() {
+    return this.page.getByRole('table');
+  }
+
+  get rows() {
+    return this.table.getByRole('row');
+  }
+
+  row(index: number) {
+    return new Row(this.page, this.rows.nth(index));
+  }
+}
+
+class Row extends BasePageModel {
+  row: Locator;
+  constructor(page: Page, row: Locator) {
+    super(page);
+    this.row = row;
+  }
+}
+
+export default ItemDetailsTable;
